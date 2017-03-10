c355906c004e49bae45590884429f7d5b61b3ff7
Ali Abdulhussein <aa3898@redbull.xdin.com>
2017-03-10 10:38:31 +0100

    UI desgin and read data from model

--- src/app/massage/massage.component.ts
+++ src/app/massage/massage.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../model/message';
+
 
 @Component({
   selector: 'app-massage',
   templateUrl: './massage.component.html',
   styleUrls: ['./massage.component.scss']
 })
+
 export class MsgPanelComponent implements OnInit {
+
+   
+  public messages:Message[];
 
   constructor() { }
 
   ngOnInit() {
+    this.initMessage();
+  }
+  
+  initMessage(){
+    this.messages=[
+    {id:1, body:"Msg01"},
+    {id:2, body:"Msg02"},
+    {id:3, body:"Msg0300"},
+    ];
+
+  }
+  
+  loadMessages(){
   }
 
+  logIt(msg:string){
+    console.log(msg);
+  }
 }
--- src/app/send-message/send-message.component.ts
+++ src/app/send-message/send-message.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./send-message.component.scss']
 })
 export class SendMessageComponent implements OnInit {
-
+  
+  
   constructor() { }
 
   ngOnInit() {
+    // alert("ngOnInit()"); 
+  }
+  sendMessage(){
+    this.logIt("Send message...!");
   }
 
+  logIt(msg:string){
+    console.log(msg);
+  }
 }
